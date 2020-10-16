14070e35dd1c0fe1cdd65d88a7fa3bbed7670e26
Oda-T <oda444567@hotmail.com>
2020-10-16 13:06:18 +0800

    init git

--- push.sh
+++ push.sh
@@ -6,6 +6,6 @@ git add -A
 
 git commit -m 'init git'
 
-git push https://github.com/TCT-FontDev/TianshuProject.git master:master
+git push -f https://github.com/TCT-FontDev/TianshuProject.git master:master
 
 cd
